84857145657bcb8b15b59ab7ae1030ad7e44039e
core.pager=cat <core.pager=cat@gmail.com>
2023-08-15 13:16:03 +0100

    https://github.com/KhRania/BookApp.git

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  StyleSheet,
+  TextInput,
+  Text,
+  View,
+  ScrollView,
+  SafeAreaView,
+  StatusBar,
+  FlatList,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import Sommaire from "./assets/Sommaire.json";
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { TouchableOpacity } from "react-native";
-
+import { useNavigation } from "@react-navigation/native";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [data, setData] = useState([]);
-  console.log(Sommaire);
-
-
 
   useEffect(() => {
     fetch(Sommaire) //API
@@ -22,7 +27,6 @@ export default function App() {
       .catch((error) => console.error(error));
   }, []);
 
-  console.log(data);
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -31,11 +35,8 @@ export default function App() {
       </Stack.Navigator>
     </NavigationContainer>
   );
-
 }
 
-
-// partie CSS
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -46,17 +47,36 @@ const styles = StyleSheet.create({
 });
 
 function HomeScreen({ navigation }) {
+  const [searchInput, onSearch] = React.useState("");
+  const [searchResult, setSearchResult] = React.useState([]);
+
+  useEffect(() => {
+    const titles = Sommaire.Titles.map((t) => t.title);
+
+    const searchResults = titles.filter((t) => t.toLowerCase().includes(searchInput.toLowerCase()));
+
+    setSearchResult(searchResults);
+  }, [searchInput]);
+
   return (
     <View style={{ flex: 1, padding: 24 }}>
-      <Text style={{ fontSize: 18, color: "green", textAlign: "center" }}>
-        MedicalBook{" "}
-      </Text>
+      {/* <ReactJson src={Sommaire} /> */}
 
+      <TextInput
+        onChangeText={onSearch}
+        style={{
+          height: 40,
+          margin: 12,
+          borderWidth: 1,
+          padding: 10,
+        }}
+        placeholder="Search here"
+      />
       <View
         style={{
           flex: 1,
           flexDirection: "column",
-          justifyContent: "space-between",
+          // justifyContent: "space-between",
         }}
       >
         <Text style={{ fontSize: 18, color: "green", textAlign: "center" }}>
@@ -72,11 +92,16 @@ function HomeScreen({ navigation }) {
         >
           Chapitres:
         </Text>
-        {Sommaire["Titles"].map((item, id) => (
-          <TouchableOpacity key={id} onPress={() => navigation.navigate('Subtitles', {
-            titleId: item.title,
-          })}>
-            <Text>{item.title}</Text>
+        {searchResult.map((item, idx) => (
+          <TouchableOpacity
+            key={idx}
+            onPress={() =>
+              navigation.navigate("Subtitles", {
+                titleId: item,
+              })
+            }
+          >
+            <Text>{item}</Text>
           </TouchableOpacity>
         ))}
       </View>
@@ -88,28 +113,136 @@ function SubtitlesScreen({ route, navigation }) {
   const { titleId } = route.params;
   const [data, setData] = useState([]);
 
+  const [searchInput, onSearch] = React.useState("");
+  const [searchResult, setSearchResult] = React.useState([]);
+
+ 
+
   function getTitleSubtitles() {
     let activeTitle = Sommaire.Titles.find((item) => item.title === titleId);
     return activeTitle.Subtitles;
   }
-  
+
   useEffect(() => {
-   let subs=  getTitleSubtitles()
-   setData(subs)
+    let subs = getTitleSubtitles();
+    setData(subs);
   }, []);
 
-  console.log(getTitleSubtitles());
+  useEffect(() => {
+    const subtitles = data.map((t) => t.subtitle);
+    const searchResults = subtitles.filter((t) => t.toLowerCase().includes(searchInput.toLowerCase()));
 
+    setSearchResult(searchResults);
+  }, [searchInput,data]);
+console.log(data);
   return (
     <View style={{ flex: 1, padding: 24 }}>
-      <Text style={{ fontSize: 18, color: "green", textAlign: "center" }}>
-       Hello
-      </Text>
-      {data.map((item, idx) => (
-          <View>
-            <Text>{item.subtitle}</Text>
-          </View>
-        ))}
+        <TextInput
+        onChangeText={onSearch}
+        style={{
+          height: 40,
+          margin: 12,
+          borderWidth: 1,
+          padding: 10,
+        }}
+        placeholder="Search here"
+      />
+
+      {searchResult.map((item, idx) => (
+        <View>
+          <Text>{item}</Text>
+        </View>
+      ))}
     </View>
   );
 }
+
+// function Filtrage  ()  {
+//   const [filterData, setfilterData] = useState ([]);
+//   const [masterData, setmasterData] = useState ([]);
+//   const [search , setsearch] = useState ('');
+
+//   useEffect (() => {
+//     fetchPosts ();
+//     return ()=> {
+
+//     }
+//   },[])
+//  const fetchPosts =() => {
+//   fetch (Sommaire)
+//   .then((response)=> response.json())
+//   .then((responseJson)=> {
+//     setfilterData(responseJson);
+//     setmasterData(responseJson);
+//   }).catch((error) => {
+//     console.error(error);
+//   })
+//  }
+
+//  const searchFilter =(text) => {
+//   if(text){
+//     const newData = masterData.filter((item) => {
+//       const itemData = item.idx ? item.idx.toUpperCase ()
+//       : ''.toUpperCase();
+//       const textData = text.toUpperCase();
+//       return itemData.indexOf(textData) > -1 ;
+//     });
+//     setfilterData(newData);
+//     setsearch(text);
+//   } else {
+//     setfilterData(masterData);
+//     setsearch(text);
+//   }
+//  }
+//  const ItemView = ( {item}) => {
+//   return(
+//     <Text style ={styles.itemStyle}>
+//       {item.id}{'. '}{item.idx.toUpperCase()}
+//     </Text>
+//   )
+//  }
+//  const ItemSeparatorView = () => {
+//   return(
+//     <View style = { {height : 0.5 , width : '100%' , backgroundColor: '#c8c8c8'}}/>
+//   )
+//  }
+//  return(
+//   <SafeAreaView style={ {flex: 1}}>
+//     <View style ={styles.container} >
+//       <TextInput
+//       style={styles.TextInputStyle}
+//       value={search}
+//       placeholder="Search "
+//       onChangeText={(Text) => searchFilter(text)}
+//       />
+
+//       <FlatList
+//       data={filterData}
+//       keyExtractor={(item, index) => index.toString()}
+//        ItemSeparatorComponent={ItemSeparatorView}
+//        renderItem={ItemView}
+//       />
+
+//     </View>
+//   </SafeAreaView>
+
+//  );
+//  const styles = StyleSheet.create (
+//   {
+//     container: {
+//       backgroundColor : 'white',
+//          },
+//          itemStyle :{
+//           padding :15
+//          },
+//          TextInputStyle : {
+//           height : 60,
+//           borderWidth : 1,
+//           paddingLeft : 20 ,
+//           margin : 5,
+//           borderColor : '#009688',
+//           backgroundColor:'white'
+//          }
+//   }
+//  )
+// }
